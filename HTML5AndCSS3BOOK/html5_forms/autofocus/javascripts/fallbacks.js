3c22ef7b74b875ec701fe99e38385e6d797aca0b
mlin <491601759@qq.com>
2019-12-07 10:40:48 -0500

    20191207-2340

---
+++ HTML5AndCSS3BOOK/html5_forms/autofocus/javascripts/fallbacks.js
@@ -0,0 +1,17 @@
+var applyColorPicker = function(){
+    $('input[type=color]').simpleColor();
+};
+
+Modernizr.load({
+    test: Modernizr.color,
+    nope: "javascripts/jquery.simple-color.js",
+    callback: function(url, result){
+        if (!result){
+            applyColorPicker();
+        }
+    }
+  });
+
+if (!Modernizr.autofocus){
+    $('input[autofocus]').focus();
+}

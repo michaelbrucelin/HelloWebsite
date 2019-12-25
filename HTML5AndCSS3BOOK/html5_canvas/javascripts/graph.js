523c4c1993034cf7d6695d6e351da992156ab5da
mlin <491601759@qq.com>
2019-12-25 09:39:10 -0500

    20191225-2239

--- HTML5AndCSS3BOOK/html5_canvas/javascripts/graph.js
+++ HTML5AndCSS3BOOK/html5_canvas/javascripts/graph.js
@@ -1,11 +1,3 @@
-/***
- * Excerpted from "HTML5 and CSS3 Second Edition",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/bhh52e for more book information.
-***/
 var canvasGraph = function(){
   var title = $('#graph_data h1').text(); 
   var labels = $("#graph_data>ul>li>p[data-name]").map(function(){  

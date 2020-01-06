b362d0c434bce00c61c3f9171d7f3ec0f85a0c52
mlin <491601759@qq.com>
2020-01-06 08:31:37 -0500

    20200106-2131

--- JavaScriptDOMStudy/test/javascripts/example.js
+++ JavaScriptDOMStudy/test/javascripts/example.js
@@ -5,9 +5,14 @@ function popUp() {
     window.open("http://www.pinger.cc", "popup", "width=320,height=480");
 }
 
+function popUp2() {
+    window.open(winURL, "popup", "width=320,height=480");
+}
+
 function popUpRegist(){
     var btn = document.getElementById("btnPopUp");
-    btn.setAttribute("onclick", "popUp();")
+    //btn.setAttribute("onclick", "popUp();")
+    btn.onclick = popUp;
 }
 
 window.onload = popUpRegist;

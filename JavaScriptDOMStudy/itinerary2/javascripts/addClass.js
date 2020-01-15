39bb8750715455f73e403654856d6d6dd2c9c381
mlin <491601759@qq.com>
2020-01-15 09:16:07 -0500

    20200115-2216

---
+++ JavaScriptDOMStudy/itinerary2/javascripts/addClass.js
@@ -0,0 +1,10 @@
+function addClass(element, value) {
+    if (!element.className) {
+        element.className = value;
+    } else {
+        newClassName = element.className;
+        newClassName += " ";
+        newClassName += value;
+        element.className = newClassName;
+    }
+}

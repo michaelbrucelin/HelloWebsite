d2ed5c368fe491e9dcd67a83b2032d3b71059eb8
mlin <michaelbrucelin@163.com>
2020-04-09 23:45:11 +0000

    20200410-0745

--- MyTest/testjsscripts/demo01.js
+++ MyTest/testjsscripts/demo01.js
@@ -3,3 +3,9 @@ console.log(typeof(arg1));
 
 var arg2 = parseFloat('10.24');
 console.log(typeof(arg2));
+
+var arg3 = Number('1024')
+var arg4 = Number('abcd')
+console.log(typeof(arg3))
+console.log(arg4)
+console.log(typeof(arg4))

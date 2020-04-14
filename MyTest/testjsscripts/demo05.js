4caa54a1293f9649402f52cc476b9c92688c6441
mlin <michaelbrucelin@163.com>
2020-04-14 00:10:17 +0000

    20200414-0810

--- MyTest/testjsscripts/demo05.js
+++ MyTest/testjsscripts/demo05.js
@@ -5,3 +5,14 @@ arr[1] = "b";
 arr[3] = "d";
 
 console.log(arr);
+console.log(arr.join());
+console.log(arr.join(';'));
+console.log(arr.toString());
+
+var arr2 = ["A", "B", "C", "D"];
+for (let i = 0; i < arr2.length; i++) {
+    console.log(i);
+}
+for (let item in arr2) {
+    console.log(item);
+}

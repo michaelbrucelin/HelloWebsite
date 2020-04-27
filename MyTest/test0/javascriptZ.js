49725398d9c9fcc9422395e6b67eaa0ab249de5f
mlin <michaelbrucelin@163.com>
2020-04-27 23:55:39 +0000

    20200428-0755

--- MyTest/test0/javascriptZ.js
+++ MyTest/test0/javascriptZ.js
@@ -248,12 +248,14 @@ window.onload = () => {
         tdpoints[i].style.cursor = "pointer";
         tdpoints[i].id = i;
         tdpoints[i].onmouseover = () => {
-            var index = this.id;
-            for (let m = 0; m < parseInt(index); m++) {
-                tdpoints[i].innerText = "★";
-            }
-            for (let n = parseInt(index) + 1; i < tdpoints.length; i++) {
-                tdpoints[i].innerText = "☆";
+            var index = parseInt(event.srcElement.id);
+            for (let j = 0; j < tdpoints.length; j++) {
+                if (j <= index) {
+                    tdpoints[j].innerText = "★";
+                }
+                else {
+                    tdpoints[j].innerText = "☆";
+                }
             }
         }
     }

51d3b7111d9cf31efdb257674eadfaa8d8d53656
mlin <michaelbrucelin@163.com>
2020-05-04 00:06:08 +0000

    20200504-0806

--- MyTest/testjsscripts/demo09.js
+++ MyTest/testjsscripts/demo09.js
@@ -5,7 +5,8 @@ var regex = /CS\d{5}/;               //这样声明更简洁，不需要转义
 //test()相当于C#中的IsMatch()
 console.log(regex.test("aCS12345b"));
 
-//exec()相当于C#中的Match()和Matches()，具体是只提取第一组还是提取所有组，取决于正则的声明，而不是函数的调用
+//exec()相当于C#中的Match()和Matches()
+//具体是只提取第一组还是提取所有组，取决于正则的声明(/g)，而不是函数的调用
 console.log(regex.exec("aCS12345b"));
 
 var regex = /CS\d{5}/i;  //忽略大小写
@@ -21,15 +22,21 @@ str.match()    //匹配模式
 str.search()   //查找
 
 //一些示例
-var regex = /(.+)@((.+)\.(.+))/;
-var str = "123@abc.com";
-var match = str.match(regex);
-console.log("================================");
-for (let i = 0; i < match.length; i++) {
+var regex1 = /(.+)@((.+)\.(.+))/;
+var str1 = "123@abc.com";
+var match1 = str1.match(regex1);
+console.log("================逐个输出================");
+console.log(RegExp.$i);
+console.log(RegExp.$i);
+console.log(RegExp.$i);
+console.log(RegExp.$i);
+console.log(RegExp.$i);
+console.log("================这样放在循环中无效================");
+for (let i = 0; i < match1.length; i++) {
     console.log(RegExp.$i);
 }
-console.log("================================");
-console.log(match);
-for (let i = 0; i < match.length; i++) {
-    console.log(match[i]);
+console.log("================这样放在循环中才有效================");
+console.log(match1);
+for (let i = 0; i < match1.length; i++) {
+    console.log(match1[i]);
 }

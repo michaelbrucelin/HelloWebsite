78a4ec81466fd1a99a96b5614fdbedc636eeb2b7
mlin <michaelbrucelin@163.com>
2020-05-18 23:56:11 +0000

    20200519-0756

--- MyTest/testjqscripts/demo02.js
+++ MyTest/testjqscripts/demo02.js
@@ -1,5 +1,11 @@
-//import "./node_modules/jquery/dist/jquery.min.js"
+const jsdom = require('jsdom');
+const {JSDOM} = jsdom;
+const {document} = (new JSDOM('<!doctype html><html><body></body></html>')).window;
+global.document = document;
+const window = document.defaultView;
+const $ = require('jquery')(window);
 
+//jquery中可以使用map方法遍历数组
 var arr = [1, 2, 3, 4];
 console.log(arr);
 
@@ -7,3 +13,5 @@ arr = $.map(arr, function (item) {
     return item * item;
 });
 console.log(arr);
+
+//自己使用js模拟一个map方法

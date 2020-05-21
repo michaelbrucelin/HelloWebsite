b2e1cdb88392b7c9ba4011d3e10354d17054f4d6
mlin <michaelbrucelin@163.com>
2020-05-21 00:03:17 +0000

    20200521-0803

--- MyTest/testjsscripts/demo13.js
+++ MyTest/testjsscripts/demo13.js
@@ -9,6 +9,7 @@ var obj = {
     }
 }
 
+
 console.log("======== Sample 01 ========");
 console.log(obj.objAge);
 obj.myFun();
@@ -20,3 +21,22 @@ function myshow() {
 
 console.log("======== Sample 02 ========");
 myshow();
+
+
+var name = '张三', age = 18;
+var obj = {
+    name: '李四',
+    objAge: this.age,
+    myFun: function () {
+        console.log(this.name + "的年龄是：" + this.age);
+    }
+}
+var db = {
+    name: '弗丁',
+    age: 99
+}
+
+console.log("======== Sample 03 ========");
+obj.myFun.call(db);
+obj.myFun.apply(db);
+obj.myFun.bind(db);

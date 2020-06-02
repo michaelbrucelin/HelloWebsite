79681508c0f3418e403f26b4c03df92674b61e12
mlin <michaelbrucelin@163.com>
2020-06-02 00:20:33 +0000

    20200602-0820

--- MyTest/test0/jqueryZ.js
+++ MyTest/test0/jqueryZ.js
@@ -4,3 +4,16 @@ $(function () {
         $(this).nextAll().text("☆");
     })
 })
+
+$(function () {
+    //这样写每次都要循环设置兄弟节点的样式，效率略低，下面的写法更有优势
+    //$("#div-li2 li").mouseover(function(){
+    //$(this).css("background", "red").siblings().css("background", "");
+    //})
+    $("#div-li2 li").mouseover(function () {
+        $(this).css("background", "red");
+    });
+    $("#div-li2 li").mouseout(function () {
+        $(this).css("background", "");
+    });
+})

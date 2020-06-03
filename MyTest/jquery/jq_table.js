5f2c19a90e30cc91697eab56aebd08650adf07f8
mlin <michaelbrucelin@163.com>
2020-06-03 23:47:49 +0000

    20200604-0747

--- MyTest/jquery/jq_table.js
+++ MyTest/jquery/jq_table.js
@@ -1,4 +1,4 @@
 $(function () {
     $("#tb01, #tb01 td, #tb01 th").css("border", "1px solid #000000").css("text-align", "center");
-    //$("#tb01 tr:eq(0)").css({ "font-size": "30px", "text-align": "left" });
+    $("#tb01 tr:eq(0)").css({ "font-size": "16px", "color": "#FF0000" });
 })

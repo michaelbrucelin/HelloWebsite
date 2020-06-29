c57572c60f4d7817b2ab4439a95a33601a66cbb3
mlin <michaelbrucelin@163.com>
2020-06-29 00:25:30 +0000

    20200629-0825

---
+++ MyTest/jquery/jq_pluginInside.js
@@ -0,0 +1,20 @@
+//JQuery插件原理
+//添加一个“静态方法”
+$.extend({
+    "myMax": function (x, y) {
+        return x > y ? x : y;
+    },
+    "myMin": function () {
+        return x > y ? y : x;
+    }
+})
+
+//添加一个“实例方法”
+$.fn.extend({
+    "myChecked": function () {
+        $(this).attr("checked", true);
+    },
+    "myUnChecked": function () {
+        $(this).attr("checked", false);
+    }
+})

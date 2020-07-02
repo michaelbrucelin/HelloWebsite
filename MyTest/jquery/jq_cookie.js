5d1106bf9d769fb6ca9f831a936f0c08766ebe39
mlin <michaelbrucelin@163.com>
2020-07-02 00:39:51 +0000

    20200702-0839

---
+++ MyTest/jquery/jq_cookie.js
@@ -0,0 +1,9 @@
+function MySetCookie(key, value) {
+    //$.cookie('name', 'value', { expires: 7, path: '/' });
+    $.cookie('name', 'value');
+}
+
+function MyGetCookie(key) {
+    console.log("wage45hyerwagteaw");
+    $("#valueGet").value = 100;
+}

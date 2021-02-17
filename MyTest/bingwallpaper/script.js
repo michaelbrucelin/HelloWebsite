aafaf54a9283a8ab4eb8f9e983c66a9ffc7c0dd3
mlin <michaelbrucelin@163.com>
2021-02-17 03:42:23 +0000

    20210217-1142

--- MyTest/bingwallpaper/script.js
+++ MyTest/bingwallpaper/script.js
@@ -19,4 +19,23 @@ function GetJSON() {
 
 window.onload = function () {
     GetJSON()
+    //PullBackground()
+}
+
+function PullBackground() {
+    var ajaxRequest = new XMLHttpRequest(), background = '';
+    ajaxRequest.open('POST', "http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US", true);
+    ajaxRequest.setRequestHeader("Connection", "close");
+    ajaxRequest.send('');
+    ajaxRequest.onreadystatechange = function () {
+        if (ajaxRequest.readyState == 4) {
+            background = ajaxRequest.responseText;
+            var res = background.split("<url>");
+            var res1 = res[1].split("</url>");
+            background = res1[0];
+
+            document.getElementById('background').style.backgroundImage = "url('http://bing.com" + background + "')"
+            document.getElementById('background').style.backgroundSize = "100%";
+        }
+    }
 }

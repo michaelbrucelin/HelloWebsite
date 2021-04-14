0082b26e7df9e042d863150f59422f1fda7ced7e
micha <michaelbrucelin@gmail.com>
2021-04-14 00:53:37 +0000

    20210414-085337

--- JavaScriptDOMStudy/animation2/javascripts/prepareSlideshow2.js
+++ JavaScriptDOMStudy/animation2/javascripts/prepareSlideshow2.js
@@ -1,4 +1,4 @@
-function prepareSlideshow() {
+function prepareSlideshow2() {
     if (!document.getElementsByTagName) { return false; }
     if (!document.getElementById) { return false; }
 
@@ -7,21 +7,16 @@ function prepareSlideshow() {
     var slideshow = document.createElement("div");
     slideshow.setAttribute("id", "slideshow");
     var preview = document.createElement("img");
-    preview.setAttribute("src", "../images/topics.gif");
+    preview.setAttribute("src", "./images/topics.gif");
     preview.setAttribute("alt", "building blocks of web design");
     preview.setAttribute("id", "preview");
-    slideshow.appendChild(preview);
-    var list = document.getElementById("linklist");
-    insertAfter(slideshow,list);
-
-    if (!document.getElementById("preview")) { return false; }
-
-    var preview = document.getElementById("preview");
     preview.style.position = "absolute";
     // preview.style.left = "0px";  //在moveElement中做了限制
     // preview.style.top = "0px";   //在moveElement中做了限制
+    slideshow.appendChild(preview);
+    var list = document.getElementById("linklist");
+    insertAfter(slideshow, list);
 
-    
     var links = list.getElementsByTagName("a");
 
     links[0].onmouseover = function () {
@@ -35,4 +30,4 @@ function prepareSlideshow() {
     }
 }
 
-addLoadEvent(prepareSlideshow);
+addLoadEvent(prepareSlideshow2);

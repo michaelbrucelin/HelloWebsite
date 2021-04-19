89833a20c85c0de7bdd86773f1be3585fa743d33
micha <michaelbrucelin@gmail.com>
2021-04-19 09:59:48 +0000

    20210419-175948

---
+++ MyTest/javascript/jquery/test/php/jsonp.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-type: application/json');
+//获取回调函数名
+$jsoncallback = htmlspecialchars($_REQUEST ['jsoncallback']);
+//json数据
+$json_data = '["customername1","customername2"]';
+//输出jsonp格式的数据
+echo $jsoncallback . "(" . $json_data . ")";
+?>
--- MyTest/javascript/jquery/test/test03.js
+++ MyTest/javascript/jquery/test/test03.js
@@ -42,3 +42,37 @@ $("#ajaxpost1").click(function () {
             alert("数据: \n" + data + "\n状态: " + status);
         });
 });
+
+//noConflict() 01
+$.noConflict();
+jQuery(document).ready(function () {
+    jQuery("#noconflict1").click(function () {
+        jQuery("#noconflictp1").text("jQuery 仍然在工作 01!");
+    });
+});
+var $ = jQuery.noConflict();  //取消noConflict()，否则影响后面的示例代码的运行，正常情况下不需要这行代码
+
+//noConflict() 02
+var jq = $.noConflict();
+jq(document).ready(function () {
+    jq("#noconflict2").click(function () {
+        jq("#noconflictp1").text("jQuery 仍然在工作 02!");
+    });
+});
+var $ = jQuery.noConflict();  //取消noConflict()，否则影响后面的示例代码的运行，正常情况下不需要这行代码
+
+//noConflict() 03
+$.noConflict();
+jQuery(document).ready(function ($) {
+    $("#noconflict3").click(function () {
+        $("#noconflictp1").text("jQuery 仍然在工作 03!");
+    });
+});
+var $ = jQuery.noConflict();  //取消noConflict()，否则影响后面的示例代码的运行，正常情况下不需要这行代码
+
+//重置
+$(document).ready(function () {
+    $("#noconflict4").click(function () {
+        jQuery("#noconflictp1").text("这是一个段落。");
+    });
+});
